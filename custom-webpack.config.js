a30e09e6b3b1feb159904cfd476a801290038dd0
Geoff Rich <geoff.rich@alaskaair.com>
2019-11-12 16:01:42 -0800

    Web components working in IE11

---
+++ custom-webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const pkg = require('./package.json');
+const path = require('path');
+
+module.exports = (config, options) => {
+  /**
+   * We need to add a rule to compile the lit-element module (which is 
+   * shipped in es6 format) to es5
+   */
+  config.module.rules.push({
+    test: /\.js$/,
+    include: [
+      path.resolve(__dirname, 'node_modules/lit-element'),
+      path.resolve(__dirname, 'node_modules/lit-html'),
+      path.resolve(__dirname, 'node_modules/@alaskaairux')
+    ],
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            '@babel/plugin-transform-runtime',
+            {
+              "corejs": false,
+              "helpers": false,
+              "regenerator": true,
+              "useESModules": false
+            }
+          ]
+        ]
+      }
+    }
+  });
+  return config;
+};
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,23 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import '@alaskaairux/ods-button';
+
+declare global {
+  interface Window {
+    WebComponents: any;
+  }
+}
+
+window.WebComponents.waitFor(() => {
+  // At this point we are guaranteed that all required polyfills have
+  // loaded, and can use web components API's.
+  // The standard pattern is to load element definitions that call
+  // `customElements.define` here.
+  // Note: returning the import's promise causes the custom elements
+  // polyfill to wait until all definitions are loaded and then upgrade
+  // the document in one batch, for better performance.
+  return import('@alaskaairux/ods-button');
+});
 
 
 if (environment.production) {
