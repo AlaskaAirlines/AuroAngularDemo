b43bf105ea622146ae9f5561d3023c317d46c1a7
Brian Dahl <brian.dahl@alaskaair.com>
2022-04-01 15:27:23 -0700

    add radio buttons

--- src/app/complex-form/complex-form.component.ts
+++ src/app/complex-form/complex-form.component.ts
@@ -11,8 +11,7 @@ export class ComplexFormComponent {
   firstName: AbstractControl;
   middleName: AbstractControl;
   lastName: AbstractControl;
-  departureAssist: AbstractControl;
-  arrivalAssist: AbstractControl;
+  desert: AbstractControl;
 
   testVal: boolean;
 
@@ -21,14 +20,12 @@ export class ComplexFormComponent {
       firstName: ['', [Validators.required]],
       middleName: ['', []],
       lastName: ['', [Validators.required]],
-      departureAssist: [true, [Validators.required]],
-      arrivalAssist: [false, [Validators.required]],
+      desert: ['no', [Validators.required]],
     });
     this.firstName = this.formGroup.controls.firstName;
     this.middleName = this.formGroup.controls.middleName;
     this.lastName = this.formGroup.controls.lastName;
-    this.departureAssist = this.formGroup.controls.departureAssist;
-    this.arrivalAssist = this.formGroup.controls.arrivalAssist;
+    this.desert = this.formGroup.controls.desert;
 
     this.testVal = true;
   }
--- src/webcomponents.ts
+++ src/webcomponents.ts
@@ -4,3 +4,5 @@ import "@aurodesignsystem/auro-menu";
 import "@aurodesignsystem/auro-input";
 import "@alaskaairux/auro-checkbox";
 import "@alaskaairux/auro-checkbox/dist/auro-checkbox-group";
+import "@alaskaairux/auro-radio";
+import "@alaskaairux/auro-radio/dist/auro-radio-group";
